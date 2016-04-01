d7021437e5e96fc9215fe26995548aabab06ae93
Giordan Escalona <Gomputer@Giordans-Air.home>
2016-04-01 19:26:23 -0400

    Updated up to date

--- Week 4/Javascript/Part2.js
+++ Week 4/Javascript/Part2.js
@@ -1,12 +1,16 @@
 function madlibs() {
-    $( "div" ).removeClass("invis");
-        setTimeout(function(){
-            for (x = 0; x <= 6; x++) {
-                var input = $('#in' + x);
-                var output = $('#out' + x);
-                output.text(input.val());
-            }
-            
-        }, 5000);
+        if ($("input").val() == ""){
+            alert("Error, virus.  I meant, you need to fill in the spaces!");
+        }else{
+            $( "div" ).removeClass("invis");
+            setTimeout(function(){
+                for (x = 0; x <= 6; x++) {
+                    var input = $('#in' + x);
+                    var output = $('#out' + x);
+                    output.text(input.val());
+                }
+                
+            }, 5000);
+        }  
         
 }
